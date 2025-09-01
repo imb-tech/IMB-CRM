3f7ce8be68ae26d2d778fe4c985b3509fee7433d
Doniyor Eshmamatov <edoniyorbekdev@gmail.com>
2025-09-01 15:45:46 +0500

    test

--- src/pages/settings/branches/index.tsx
+++ src/pages/settings/branches/index.tsx
@@ -9,6 +9,7 @@ import ParamPagination from "@/components/as-params/pagination"
 import { useSearch } from "@tanstack/react-router"
 import SettingsHeader from "../settings-header"
 import { useStore } from "@/hooks/use-store"
+import useMe from "@/hooks/useMe"
 
 const BranchesMain = () => {
     const { openModal: openModalBranch } = useModal(`${BRANCH}-add`)
