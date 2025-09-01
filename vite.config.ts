58bef1d3fde3cf07cb16fe5ef37bb7a8df8cb76b
Doniyor Eshmamatov <edoniyorbekdev@gmail.com>
2025-09-01 13:33:29 +0500

    prod

--- vite.config.ts
+++ vite.config.ts
@@ -13,22 +13,5 @@ export default defineConfig({
     server: {
         host: true,
         port: 3002,
-        proxy: {
-            "/api": {
-                target: loadEnv("development", process.cwd(), '').VITE_DEFAULT_URL, // backend manzili
-                changeOrigin: true,
-                secure: false,
-                rewrite: (path) => path.replace(/^\/api/, ""),
-                configure: (proxy) => {
-                    proxy.on("proxyReq", (_, req, res) => {
-                        if (req.headers.accept?.includes("text/html")) {
-                            res.writeHead(404, { "Content-Type": "text/html" })
-                            res.end('<h1 style="display: flex; justify-content:center; align-items:center; height: 100vh; font-size: 58px;">PAGE NOT FOUND!</h1>')
-                        }
-                    })
-                },
-
-            },
-        },
     }
 })
